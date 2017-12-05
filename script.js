71eac7613442aa226b0957e5f3db2376c1baa716
Sielarz <34028988+Sielarz@users.noreply.github.com>
2017-12-05 11:11:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var a=10; //podstawa trojkata
-var h=5;  //wysokosc trojkata
+var a = 10; //podstawa trojkata
+var h = 5;  //wysokosc trojkata
 var triangleArea = a * h / 2; //obliczanie pola trojkata
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
 
